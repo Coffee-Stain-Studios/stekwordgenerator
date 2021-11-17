132c3754e9a472dbe67d6e099307284bd22acb74
Gnutt Halvordsson <gnutt@halvordsson.se>
2021-11-17 11:42:54 +0100

    Added help documentation

--- stekWords.ps1
+++ stekWords.ps1
@@ -1,17 +1,19 @@
-param([Int]$Count=5, [switch]$Verbose)
+<#
+.DESCRIPTION
+A password generator creating passwords that are pronouncable and rememberable. Not for secure usage
 
-if( $Verbose )
-{
-	$VerbosePreference = "Continue"
-}
+.PARAMETER Count
+Number of passwords to generate
+#>
+[CmdletBinding()]
+param([Int]$Count = 5)
 
-function capitaliseFirst
-{
+function capitaliseFirst {
 	param( [String]$String )
-	return $String.substring(0,1).toUpper()+$String.substring(1).toLower()
+	return $String.substring(0, 1).toUpper() + $String.substring(1).toLower()
 }
 
-# Read dictionaries
+Write-Verbose -Message "Reading the three dictionaries"
 $doWords = Get-Content doWords.txt
 $describeWords = Get-Content describeWords.txt
 $actorWords = Get-Content actorWords.txt
@@ -20,25 +22,20 @@ $actorWords = Get-Content actorWords.txt
 $lowestNumber = 100
 $highestNumber = 999
 
+Write-Verbose -Message "Calculating the maximum number of possible passwords"
 $maxCount = $doWords.Count * $describeWords.Count * $actorWords.Count * ( $highestNumber - $lowestNumber )
 
-if( $Verbose )
-{
-	Write-Verbose "Maximum available stekwords: $maxCount"
-}
+Write-Verbose -Message "Maximum available stekwords: $maxCount"
 
-if( $Count -gt $maxCount )
-{
+if ( $Count -gt $maxCount ) {
 	Write-Error "Count to high; highest allowed number is: $maxCount"
 	exit
 }
 
 $stekwords = [System.Collections.ArrayList]@()
 
-For( $i = 0; $i -lt $Count; $i++ )
-{
-	if( $count -gt 99 -And $i % 100 -eq 0 )
-	{
+For ( $i = 0; $i -lt $Count; $i++ ) {
+	if ( $count -gt 99 -And $i % 100 -eq 0 ) {
 		$percent = [math]::Round(($i / $Count) * 100)
 		Write-Progress -Activity "Generating $Count Stekwords" -Status "$percent% Complete" -PercentComplete $percent
 	}
@@ -50,9 +47,8 @@ For( $i = 0; $i -lt $Count; $i++ )
 	$stekWord = "$doWord$num$describeWord$actorWord"
 	
 	$found = $stekwords.IndexOf($stekWord)
-	if( $found -ne -1 )
-	{
-		Write-Verbose "Duplicate of $stekWord found"
+	if ( $found -ne -1 ) {
+		Write-Verbose -Message "Duplicate of $stekWord found"
 		$i--;
 		continue
 	}
